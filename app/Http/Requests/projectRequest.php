e4d8f37123861a7c80452ea6ac6f8b12de7f3b29
NicolaBellini <bellininicola4366@gmail.com>
2024-05-28 15:51:14 +0200

    [add] in request numeric difficulty, [add] nullable topic

--- app/Http/Requests/projectRequest.php
+++ app/Http/Requests/projectRequest.php
@@ -23,8 +23,7 @@ class projectRequest extends FormRequest
     {
         return [
         'name' => 'required|min:2|max:30',
-        'topic' => 'required|min:2|max:50',
-        'difficulty' => 'required|min:1|max:10'
+        'difficulty' => 'required|numeric|min:1|max:10'
         ];
     }
 
@@ -34,10 +33,8 @@ class projectRequest extends FormRequest
             'name.required' => 'Il nome deve essere inserito',
             'name.min' => 'Il nome deve avere almeno :min caratteri',
             'name.max' => 'Il nome deve avere massimo :max caratteri',
-            'topic.required' => 'Il topic deve essere inserito',
-            'topic.min' => 'Il topic deve avere almeno :min caratteri',
-            'topic.max' => 'Il topic deve avere massimo :max caratteri',
             'difficulty.required' => 'La difficoltà deve essere inserita',
+            'difficulty.numeric' => 'La difficoltà inserita non è un numero valido',
             'difficulty.min' => 'La difficoltà deve avere almeno :min caratteri',
             'difficulty.max' => 'La difficoltà deve avere massimo :max caratteri'
         ];
--- database/migrations/2024_05_21_130741_create_projects_table.php
+++ database/migrations/2024_05_21_130741_create_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
          Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name',100);
-            $table->string('topic',200);
+            $table->string('topic',200)->nullable();
             $table->tinyInteger('difficulty');
             $table->string('image')->nullable();
             $table->string('image_original_name')->nullable();
